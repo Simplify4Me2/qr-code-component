cd4779d785fb91847742581be92b29c26c54eb98
Simplify4Me2 <simon.vereecke@live.com>
2024-02-29 22:02:11 +0100

    delete nextjs app

--- my-qr-code-component/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // basePath: '/qr-code-component',
-    // assetPrefix: '/qr-code-component',
-    output: "export",
-    reactStrictMode: true
-};
-
-export default nextConfig;
