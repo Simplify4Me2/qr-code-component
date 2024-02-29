3754115c3591f80eff58a59ab7b50b392844043d
Simplify4Me2 <simon.vereecke@live.com>
2024-02-29 21:31:36 +0100

    moved dependencies

--- my-qr-code-component/next.config.mjs
+++ my-qr-code-component/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/qr-code-component',
+    // basePath: '/qr-code-component',
     assetPrefix: '/qr-code-component',
     output: "export",
     reactStrictMode: true
