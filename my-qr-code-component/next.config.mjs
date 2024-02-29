266dd55cffffc70ba49e26325d54c87fc463d9af
Simplify4Me2 <simon.vereecke@live.com>
2024-02-29 21:26:43 +0100

    set assetPrefix

--- my-qr-code-component/next.config.mjs
+++ my-qr-code-component/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // basePath: "/qr-code-component",
+    assetPrefix: '/qr-code-component',
     output: "export",
     reactStrictMode: true
 };
