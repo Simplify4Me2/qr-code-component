eb99b6d31646f06f2fca82519d980d0216213fb0
Simplify4Me2 <simon.vereecke@live.com>
2024-03-01 21:18:26 +0100

    implement QR code challenge

--- my-qr-code-component/src/App.tsx
+++ my-qr-code-component/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ImageQRCode from './assets/image-qr-code.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    // <></>
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='bg-sky-100 h-screen flex flex-col justify-center'>
+      <div className='flex justify-center'>
+        <div className="font-outfit bg-white flex flex-col items-center rounded-xl p-2">
+        <img src={ImageQRCode} width={200} height={37} className="logo" alt="QR Code image" />
+          <h1 className="font-bold flex flex-col items-center m-2">
+            <span>Improve your front-end</span>
+            <span>skills by building projects</span>
+          </h1>
+          <p className="font-normal text-xs text-slate-400 flex flex-col items-center mb-6">
+            <span>Scan the QR code to visit Frontend</span>
+            <span>Mentor and take your coding skills to</span>
+            <span>the next level</span>
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='text-3xl font-bold underline'>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main>
     </>
+    //   <a href="https://vitejs.dev" target="_blank">
+    //   <img src={viteLogo} className="logo" alt="Vite logo" />
+    // </a>
   )
 }
 
